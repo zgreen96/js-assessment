4f1a9b326ddd762cab37038a6140eb34f04c38d2
zgreen96 <zgreen96@gmail.com>
2019-06-23 22:15:02 -0400

    Add files via upload

--- app/async.js
+++ app/async.js
@@ -1,11 +1,39 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async: function(value) 
+  {
+  	return new Promise(function(resolve, reject)
+  	{
+  		setTimeout(function()
+  		{
+  			resolve(value), 1
+  		});
+  	})
   },
 
-  manipulateRemoteData: function(url) {
+  manipulateRemoteData: function(url) 
+  {
+  	return new Promise(function(resolve, reject)
+  	{
+  		var req = new XMLHttpRequest();
+  		req.open('GET',url);
 
+		req.onload = function()
+		{  		
+	  		if(req.status == 200)
+	  			resolve(req.response);
+	  		else
+	  			reject(Error(req.statusText));
+	  	}
+
+	  	req.send();
+
+  	}).then(function(response)
+  		{
+  			var data = JSON.parse(response).people;
+  			return data.map(key => key.name).sort();
+
+  		});
   }
 };
